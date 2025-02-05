ae863b3fa51f46eaecb0ba116c8e05faa6733158
Thabang Motswenyane <thabangmotswenyane511@gmail.com>
2025-02-05 16:22:20 +0200

    worked on the loan_approval function, to fit our vision of giving micro-loans to individuals of all income groups Ts&Cs applied

--- EDA/data_collection_and_preparation.py
+++ EDA/data_collection_and_preparation.py
@@ -4,10 +4,19 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#  Cleaning code 
+def connection_and_retrieval_of_database_info():
+    """
+        purpose of method is to create a connection 
+        to the database
+    """
+    engine = create_engine('sqlite:///HouseHold_data.db')  
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return engine
 
-engine = create_engine('sqlite:///HouseHold_data.db')  
-Session = sessionmaker(bind=engine)
-session = Session()
+
+engine = connection_and_retrieval_of_database_info()
 
 df = pd.read_sql('SELECT * FROM households', engine)
 
@@ -21,15 +30,71 @@ if 'disposable_income' not in random_applicants.columns or 'debt_to_income_ratio
     raise ValueError("Required fields 'disposable_income' and 'debt_to_income_ratio' are missing in the database.")
 
 
-def loan_approval(row):
-    if row['credit_score'] >= 650 and row['debt_to_income_ratio'] < 0.7 and row['disposable_income'] > 10000:
-        return 'Approved', 12, row['loan_amount_requested'] / 12, row['full_name']
-    elif row['credit_score'] >= 500 and row['debt_to_income_ratio'] < 0.4 and row['disposable_income'] > 3000:
-        return 'Conditionally Approved', 24, row['loan_amount_requested'] / 24, row['full_name']
+"""
+             **** TOP FIXES **** 
+
+  1. We need to identify exactly why some high income status people still get rejected for loans
+     even when they meet requirements           
+
+
+
+
+     Let us think about factors affecting the process of getting loan approved
+
+     1. credit_score
+     2. disposable_income 
+     3. loan_amount_requested
+     4. debt_to_income_ratio
+     
+
+    KEY : 
+    months_to_pay, m_t_p
+    loan_amount_requested, l_a_r
+
+
+    if loan_amount greater than 50000 to 100000 && credit_Score >=680 && debt_to_income_ratio < 0.5:
+       return approved, months_to_pay, how_much_to_pay_back_p/m = l_a_r/m_t_p, full_name
+    elif loan_amount greater than 10000 to 50000 && credit_score <=550 && debt_to_income_ratio < 0.6:
+        return approved, months_to_pay, how_much_to_pay_back_p/m = l_a_r/m_t_p, full_name
+    elif loan_amount greater than 5000 but less than 10000 and credt_score >= 500 and debt_to_income_ratio < 0.4
+        return conditionally approved, months_to_pay, how_much_to_pay_back_p/m = l_a_r/m_t_p, full_name
+    else:
+        return rejected, 0, 0, full_name
+
+"""
+
+STARTING_AMOUNT=50000
+HIGHEST_AMOUNT = 100000
+tenK = 10000
+FiveK = 5000
+
+def new_loan_approval_method(row):
+    if STARTING_AMOUNT < row['loan_amount_requested'] < HIGHEST_AMOUNT and row['credit_score'] >= 680 and row['debt_to_income_ratio'] < 0.5:
+        return 'Approved',12, row['loan_amount_requested'] / 12, row['full_name']
+    elif tenK < row['loan_amount_requested'] <=STARTING_AMOUNT and row['credit_score'] >=550 and row['debt_to_income_ratio'] < 0.6:
+        return 'Approved', 18, row['loan_amount_requested']/ 18, row['full_name']
+    elif FiveK < row['loan_amount_requested'] < tenK and row['credit_score'] >= 500 and row['debt_to_income_ratio'] < 0.4:
+        return 'Conditionally Approved',24, row['loan_amount_requested']/24, row['full_name']
+    elif row['disposable_income'] >= (tenK) and row['credit_score'] >= 500 and row['debt_to_income_ratio'] < 0.7:
+        return 'Conditionally Accepted', 48, row['loan_amount_requested']/48, row['full_name']
     else:
         return 'Rejected', 0, 0, row['full_name']
 
-random_applicants['loan_approval_status'], random_applicants['loan_repayment_period'], random_applicants['monthly_repayment'], random_applicants['full_name'] = zip(*random_applicants.apply(loan_approval, axis=1))
+
+# def loan_approval(row):
+#     if row['credit_score'] >= 650 and row['debt_to_income_ratio'] < 0.7 and row['disposable_income'] > 10000:
+#         return 'Approved', 24, row['loan_amount_requested'] / 24, row['full_name']
+#     elif row['credit_score'] >= 500 and row['debt_to_income_ratio'] < 0.5 and row['disposable_income'] > 3000:
+#         return 'Conditionally Approved', 12, row['loan_amount_requested'] / 12, row['full_name']
+#     else:
+#         return 'Rejected', 0, 0, row['full_name']
+
+
+
+
+
+
+random_applicants['loan_approval_status'], random_applicants['loan_repayment_period'], random_applicants['monthly_repayment'], random_applicants['full_name'] = zip(*random_applicants.apply(new_loan_approval_method, axis=1))
 
 loan_engine = create_engine('sqlite:///loan_applicants.db')  
 
